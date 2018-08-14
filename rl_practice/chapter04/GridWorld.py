a6fe8c952b9e725e6ac91604994f48b8dd04f88d
Kandai Watanabe <kandai.wata@gmail.com>
2018-08-15 00:12:58 +0900

    completed GridWorld in_place=False version

--- rl_practice/chapter04/GridWorld.py
+++ rl_practice/chapter04/GridWorld.py
@@ -1,114 +1,88 @@
-#######################################################################
-# Copyright (C)                                                       #
-# 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
-# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
-# Permission given to modify the code as long as you keep this        #
-# declaration at the top                                              #
-#######################################################################
-
 from __future__ import print_function
+
+import argparse
 import numpy as np
 
-WORLD_SIZE = 4
-ACTION_PROB = 0.25
-REWARD = -1
+NUM_ACTIONS = 4
 
-def next_state(state, action):
-    i, j = state
-    if i == 0 and action == 'U':
-        return [i, j]
-    if i == WORLD_SIZE - 1 and action == 'D':
-        return [i, j]
-    if j == 0 and action == 'L':
-        return [i, j]
-    if j == WORLD_SIZE - 1 and action == 'R':
-        return [i, j]
+def calc_next_state(state, p, gridsize):
+    size    = gridsize*gridsize
+    b       = np.array(range(gridsize))
+    if np.any(b[:]==state) and p==1:
+        return state
+    bound   = gridsize*b
+    if np.any(bound[:]==state) and p==4:
+        return state
+    bound   = gridsize*(b+1)-1
+    if np.any(bound[:]==state) and p==2:
+        return state
+    bound   = np.array(range(size-gridsize, size))
+    if np.any(bound[:]==state) and p==3:
+        return state
 
-    if action == 'U':
-        return [i - 1, j]
-    if action == 'D':
-        return [i + 1, j]
-    if action == 'R':
-        return [i, j + 1]
-    if action == 'L':
-        return[i, j - 1]
+    if p==1:
+        return state-gridsize
+    if p==2:
+        return state+1
+    if p==3:
+        return state+gridsize
+    if p==4:
+        return state-1
 
-states = []
-for i in range(WORLD_SIZE):
-    for j in range(WORLD_SIZE):
-        if (i == 0 and j == 0) or (i == WORLD_SIZE-1 and j == WORLD_SIZE-1):
-            continue
-        states.append([i, j])
 
-actions = ['U', 'D', 'R', 'L']
+def calc_new_value(state, values, policy, gridsize):
+    sum = 0
+    for p in policy:
+            next_state = calc_next_state(state, p, gridsize)
+            sum += 1/NUM_ACTIONS *(-1 + values[next_state])
+    return sum
 
-def compute_state_values(in_place=False):
-    new_state_values = np.zeros((WORLD_SIZE, WORLD_SIZE))
-    state_values = new_state_values.copy()
+def policy_evaluation(states, values, policy, gridsize, threshold=0.0000001):
     iteration = 1
+    new_values = values.copy()
     while True:
-        src = new_state_values if in_place else state_values
-        for (i, j) in states:
-            value = 0
-            for action in actions:
-                next_i, next_j = next_state([i, j], action)
-                value += ACTION_PROB * (REWARD + src[next_i, next_j])
-            new_state_values[i, j] = value
+        delta = 0
+        print(iteration)
+        for state in states:
+            if not (state==0 or state==gridsize*gridsize-1):
+                v       = values[state]
+                new_v   = calc_new_value(state, values, policy, gridsize)
+                new_values[state] = new_v
 
-        if np.sum(np.abs(new_state_values - state_values)) < 1e-4:
-            state_values = new_state_values.copy()
+                delta = max(delta, v-new_v)
+        if delta<threshold:
             break
 
-        state_values = new_state_values.copy()
-        iteration += 1
+        values = new_values.copy()
+        print(np.reshape(values, (gridsize, gridsize)))
+        iteration +=1
 
-    return state_values, iteration
+    return iteration
 
-if __name__ == '__main__':
-    state_values, iteration = compute_state_values(in_place=True)
-    print('In-place:')
-    print('State values under random policy after %d iterations' % (iteration))
-    print(state_values)
+def policty_improvement():
+    pass
 
-    state_values, iteration = compute_state_values(in_place=False)
-    print('Synchronous:')
-    print('State values under random policy after %d iterations' % (iteration))
-    print(state_values)
+def main():
+    parse = argparse.ArgumentParser(description='Grid World Policty Iteration')
+    parse.add_argument('--gridsize','-s', type=int, default=4,
+                        help='Size of the Grid')
+    args = parse.parse_args()
 
+    size    = args.gridsize*args.gridsize
+    States  = np.array(range(size))
+    Values   = np.zeros(size)
+    Policy  = np.array([1,2,3,4]) # up, right, bottom, left
+    print('States')
+    print(States)
+    print('Initial Value')
+    print(Values)
+    print('Policy')
+    print(Policy)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    iteration = policy_evaluation(States, Values, Policy, args.gridsize)
+    print('Iteration: %i'%(iteration))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
